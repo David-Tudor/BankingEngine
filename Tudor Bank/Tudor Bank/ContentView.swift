8c50a4436986ba7375fc16ea69eda54158e13d03
DvdTd <89341571+DvdTd@users.noreply.github.com>
2023-09-23 13:56:01 +0100

    Added textfield

--- Tudor Bank/Tudor Bank/ContentView.swift
+++ Tudor Bank/Tudor Bank/ContentView.swift
@@ -6,14 +6,31 @@
 //
 
 import SwiftUI
+import BankingEngine
 
 struct ContentView: View {
+     @State var name = ""
+    
+    let myBank = BankingEngine()
+    
+    func testBank() {
+        myBank.createAccount(id: 1, name: "David", balance: 12.39)
+        print(name)
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Enter name", text: $name)
+            Button {
+                testBank()
+                
+            } label: {
+                Text("Run testBank")
+            }
+
         }
         .padding()
     }
