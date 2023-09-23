a0b23e7329e699f7532351685e174d4f5abcd1e4
DvdTd <89341571+DvdTd@users.noreply.github.com>
2023-09-23 13:18:06 +0100

    added new multiplatform project

--- README.md
+++
@@ -1,3 +0,0 @@
-# BankingEngine
-
-A description of this package.
---
+++ Tudor Bank/Tudor Bank/Tudor_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tudor_BankApp.swift
+//  Tudor Bank
+//
+//  Created by David Tudor on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tudor_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
